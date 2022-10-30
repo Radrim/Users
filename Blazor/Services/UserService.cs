df16fdfe024e4f326fe4ab770ef7d35858e7043a
Радмир Габдрахманов <gabdrahmanov.radmir2018@gmail.com>
2022-10-31 01:59:16 +0300

    Добавьте файлы проекта.

---
+++ Blazor/Services/UserService.cs
@@ -0,0 +1,25 @@
+﻿namespace Blazor.Data
+{
+    public class UserService
+    {
+        private static string[] FirstNames = new[]
+        {
+        "Bred", "Jack", "Tom", "Sasha", "Victor", "Bruce", "Jason", "Tomas", "Sergey"
+        };
+
+        private static string[] LastNames = new[]
+        {
+        "Ivanov", "Skvorcov", "Norov", "Vovorov", "Sogin", "Mikelson", "Pit"
+        };
+
+        public static List<User> GetUserList()
+        {
+            return Enumerable.Range(1, Random.Shared.Next(1, 9)).Select(index => new User
+            {
+                FirstName = FirstNames[Random.Shared.Next(FirstNames.Length)],
+                LastName = LastNames[Random.Shared.Next(LastNames.Length)],
+                Age = Random.Shared.Next(0, 100)
+            }).ToList();
+        }
+    }
+}
